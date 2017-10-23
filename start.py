634b02dc9cce68e2eb20bb67dfed8abdf2588834
cmatzenbach <cmatzenbach@gmail.com>
2017-10-23 01:58:40 -0400

    Added class shell for square and comments from youtube video

--- start.py
+++ start.py
@@ -1,11 +1,26 @@
-import sys, pygame
+import sys, pygame, logging
 
+class Square(pygame.sprite.Sprite):
+    """ A square is an individual square that makes up the checkers board
+    Returns: square object
+    Functions: update,
+    Attributes: """
+
+    def __init__(self, x_coord, y_coord, id):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.id = id
+
+    def update(self):
+        # update functions (put crap below to validate syntax)
+        x_coord += 0
+
+    
 pygame.init()
 screen = pygame.display.set_mode((700, 700))
 done = False
 
 clock = pygame.time.Clock()
-
 
 while not done:
 
@@ -22,14 +37,21 @@ while not done:
 
         startX = 50
         startY = 50
-        pygame.draw.rect(screen, (255, 000, 000), pygame.Rect(startX, startY, 600, 600))
+        pygame.draw.rect(screen, (255, 000, 000), pygame.Rect(startX, startY, 600, 800))
 
         sqW = 75
         sqH = 75
+
+        # generate IDs:
+        # blue_blobs = dict(enumerate([Classname(VALUES) for i in range(SOME_RANGE)]))
+
         for i in range(0, 8):
             shift = 75 if (i % 2) == 0 else 0
             for j in range (0, 4):
                 pygame.draw.rect(screen, (000, 000, 000), pygame.Rect(startX + shift + (150 * j), startY + (75 * i), sqW, sqH))
+
+        # for use later - function below spits out coords of mouse anytime it is moved by the user
+        logging.warning(pygame.mouse.get_pos())
 
     pygame.display.flip()
     clock.tick(60)
