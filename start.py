23bca5d470f7ecfda424f1876b798161141dd1d0
cmatzenbach <cmatzenbach@gmail.com>
2017-10-23 21:22:18 -0400

    Setup class shells

--- start.py
+++ start.py
@@ -1,5 +1,14 @@
 import sys, pygame, logging
 
+class Board ():
+    """ Game board for checkers
+    Returns: board object
+    Functions: update,
+    Attributes """
+
+    def __init__(self, status):
+        self.status = status
+
 class Square(pygame.sprite.Sprite):
     """ A square is an individual square that makes up the checkers board
     Returns: square object
@@ -15,7 +24,7 @@ class Square(pygame.sprite.Sprite):
         # update functions (put crap below to validate syntax)
         x_coord += 0
 
-    
+
 pygame.init()
 screen = pygame.display.set_mode((700, 700))
 done = False
